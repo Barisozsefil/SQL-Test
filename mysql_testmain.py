59ec1b5cf7d43712d515b4844daf839373b4f969
Barış Özsefil <77811917+Barisozsefil@users.noreply.github.com>
2023-09-06 13:09:29 +0300

    Add files via upload
    UPDATE

--- mysql_testmain.py
+++ mysql_testmain.py
@@ -16,40 +16,74 @@ mycursor = mydb.cursor()
 
 
 # Yeni bir veritabanı oluşturma
-mycursor.execute("CREATE DATABASE testmain")
+#mycursor.execute("CREATE DATABASE testmain")
+
+# Tablo oluşturma
+#mycursor.execute("CREATE TABLE students (name VARCHAR(255), age VARCHAR(255))")
+
+# Tablo Silme 
+#mycursor.execute("DROP TABLE studentsss")
+
+# Tabloya Sütun ekleme
+#mycursor.execute ("ALTER TABLE students ADD city VARCHAR(255)")
+
+# Tablo Sutün Silme
+#mycursor.execute("ALTER TABLE students DROP COLUMN city")
 
 
-# Örnek bir Students tablo oluşturma
-mycursor.execute("CREATE TABLE students (name VARCHAR(255) , age INTEGER(10)) ")
 
+"""
+#Tablo İçeriği
+sql = "INSERT INTO custumers (name, age, city) VALUES (%s,%s,%s)"
 
-# SQL sorgusu
-sql = "INSERT INTO students (name, age) VALUES (%s, %s)"
-students1=("Baris",21)
+students1=[
+           ("Baris",21,"mars"),
+           ("Burak",21,"jupiter"),
+           ("Büşra",21,"adana"),
+           ("Bengi",21,"mersin"),
+           ("Berkay",21,"hatay"),
+]
 
-mycursor.execute(sql, students1)
+mycursor.executemany(sql, students1)
 
 mydb.commit()
 
 
 
-for x in mycursor:
+
+
+sql = ("SELECT * FROM custumers WHERE name='Bengi' ")
+
+mycursor.execute (sql)
+
+
+myresult = mycursor.fetchall()
+
+for row in myresult:
+    print(row)
+
+
+
+
+sql = "UPDATE custumers SET age = 13 WHERE name = 'Bengi' "
+
+mycursor.execute(sql)
+
+
+mydb.commit()
+
+
+
+
+
+sql = "DELETE FROM custumers WHERE name = 'Baris'"
+
+
+mycursor.execute(sql)
+
+mydb.commit()
+
+for x in sql:
     print(x)
-
-
-
-
-
-
-
-
-
-
-
-
+    
 """
-
-
-
-"""
-
